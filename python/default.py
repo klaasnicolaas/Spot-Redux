8526752bb3a7d8a7b0cbc497e4dd7ddaa3090a17
Klaas Schoute <klaas_schoute@hotmail.com>
2021-12-07 10:16:01 +0000

    Edit: servo default start position

--- python/default.py
+++ python/default.py
@@ -6,15 +6,19 @@ kit = ServoKit(channels=16)
 # Schouder max = 180
 # Schouder min = 50
 
-# Bovenbeen max = 130 (liever niet verder dan 120-130)
+# Bovenbeen max = 150 (liever niet verder dan 120-130)
 # Bovenbeen min = 20 (liever niet verder dan 50)
 
 # Onderbeen max = 110
 # Onderbeen min = 30
 
+# kit.servo[0].angle = 150
+# kit.servo[1].angle = 110 # 50 / 110
+# kit.servo[2].angle = 85 # 80 / 30
+
 # Schouder
 kit.servo[0].angle = 95
 # Bovenbeen
-kit.servo[1].angle = 120 # 50 / 110
+kit.servo[1].angle = 147 # 50 / 110
 # Onderbeen
-kit.servo[2].angle = 90 # 80 / 30
+kit.servo[2].angle = 81 # 80 / 30
