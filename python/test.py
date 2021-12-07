f6448904617d333985dee0b8d4f386a19e35a24b
Klaas Schoute <klaas_schoute@hotmail.com>
2021-12-07 10:17:50 +0000

    Edit: python test script

--- python/test.py
+++ python/test.py
@@ -1,9 +1,52 @@
 import time
+import threading
+
 from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
-while True:
-    for angle in range(50, 180, 1):
-        kit.servo[0].angle = angle
-        time.sleep(0.05)
+def bovenbeen_loop():
+    while True:
+        """Bovenbeen."""
+        for angle in range(180, 20, -1):
+            # Omlaag
+            kit.servo[1].angle = angle
+            time.sleep(0.1)
+        # for angle in range(50, 180, 1):
+        #     # Omhoog
+        #     kit.servo[1].angle = angle
+        #     time.sleep(0.1)
+
+def onderbeen_loop():
+    """Onderbeen."""
+    while True:
+        for angle in range(110, 30, -1):
+            # Omhoog
+            kit.servo[2].angle = angle
+            time.sleep(0.1)
+        # for angle in range(30, 110, 1):
+        #     # Omlaag
+        #     kit.servo[2].angle = angle
+        #     time.sleep(0.1)
+
+def schouder_loop():
+    """Schouder."""
+    while True:
+        for angle in range(80, 180, 1):
+            # Naar buiten
+            kit.servo[0].angle = angle
+            time.sleep(0.05)
+        for angle in range(180, 80, -1):
+            # Naar binnen
+            kit.servo[0].angle = angle
+            time.sleep(0.05)
+
+
+if __name__ == '__main__':
+    thread_schouder = threading.Thread(target=schouder_loop)
+    thread_bovenbeen = threading.Thread(target=bovenbeen_loop)
+    thread_onderbeen = threading.Thread(target=onderbeen_loop)
+
+    thread_schouder.start()
+    # thread_bovenbeen.start()
+    # thread_onderbeen.start()
